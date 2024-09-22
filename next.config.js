cead8bbc495495a8c97fcbf03f0065813419c216
yusing <yusing@6uo.me>
2024-09-23 05:55:57 +0800

    packages upgrade

--- next.config.js
+++ next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 
 const apiBaseURL =
-  process.env.NODE_ENV === "development"
-    ? "http://10.0.3.1:8888/v1"
-    : "http://localhost:8888/v1";
+    process.env.NODE_ENV === "development"
+        ? "http://10.0.3.1:8888/v1"
+        : "http://localhost:8888/v1";
+
+console.log(apiBaseURL);
 
 const nextConfig = {
-  output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${apiBaseURL}/:path*`, // Proxy to Backend apiBaseURL,
-      },
-    ];
-  },
+    output: "standalone",
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: `${apiBaseURL}/:path*`, // Proxy to Backend apiBaseURL,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
