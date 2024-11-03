005eda38b57a74b2af70f379b5e186a49e7e1609
yusing <yusing@6uo.me>
2024-11-03 10:34:46 +0800

    fixed incorrect footer placement

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
         >
           <div className="flex flex-col h-screen">
             <Navbar />
-            <main className="mx-auto lg:px-32 md:px-24 sm:px-12 h-full">
+            <main className="mx-auto lg:px-32 md:px-24 sm:px-12">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
