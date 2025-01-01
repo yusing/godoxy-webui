c77af24a2b4a65148f487fe7e310cf0a21f4c3eb
yusing <yusing@6uo.me>
2025-01-02 02:59:54 +0800

    fix not getting correct env api addr

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 
+const envPrefixes = ["GODOXY_", "GOPROXY_", ""];
+let apiAddr;
+
+for (const prefix of envPrefixes) {
+  apiAddr = process.env[`${prefix}API_ADDR`];
+  if (apiAddr !== undefined) {
+    break;
+  }
+}
+
 const apiBaseURL =
-  process.env.GOPROXY_API_ADDR === undefined
+  apiAddr === undefined
     ? "http://127.0.0.1:8888/v1"
-    : `http://${process.env.GOPROXY_API_ADDR}/v1`;
+    : `http://${apiAddr}/v1`;
 
 const nextConfig = {
   output: "standalone",
+
   async rewrites() {
     return [
       {
