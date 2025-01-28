83bb95757a86fafd8eea8d64f0152d718b02574d
yusing <yusing@6uo.me>
2025-01-28 11:31:10 +0800

    Enable react compiler, dependencies upgrade

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
+    reactCompiler: true,
   },
   output: "standalone",
   async rewrites() {
