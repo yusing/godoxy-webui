789349f20557bc5f2dc3c6739b7f06d200129dd5
yusing <yusing@6uo.me>
2025-02-23 14:00:50 +0800

    improved add agent dialog, ui fixes

--- src/components/metrics/add_agent_button.tsx
+++ src/components/metrics/add_agent_button.tsx
@@ -22,10 +22,11 @@ import { Code, Group, HStack, Input, Stack, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { FaDocker, FaServer } from "react-icons/fa6";
-import { LuPlus } from "react-icons/lu";
+import { LuInfo, LuPlus } from "react-icons/lu";
 import { Checkbox } from "../ui/checkbox";
 import { SegmentedControl } from "../ui/segmented-control";
 import { toaster } from "../ui/toaster";
+import { ToggleTip } from "../ui/toggle-tip";
 
 const agentTypes = {
   docker: {
@@ -38,9 +39,9 @@ const agentTypes = {
   },
 };
 
-function Label({ children }: { children: React.ReactNode }) {
+function Label({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <Text fontWeight={"medium"} textAlign={"right"} w={"70px"} mr="2">
+    <Text fontWeight={"medium"} textAlign={"left"} minW={"70px"} mr="2">
       {children}
     </Text>
   );
@@ -52,13 +53,14 @@ export function AddAgentDialogButton() {
   const [addLoading, setAddLoading] = useState(false);
   const { control, register, handleSubmit } = useForm<AddAgentForm>({
     defaultValues: {
-      type: "docker",
       name: "",
       host: "",
       port: 8890,
       nightly: false,
     },
   });
+  const [type, setType] = useState<AgentType>("docker");
+  const [explicitOnly, setExplicitOnly] = useState(false);
   const [agent, setAgent] = useState<NewAgentResponse | null>(null);
 
   return (
@@ -81,30 +83,22 @@ export function AddAgentDialogButton() {
         <DialogHeader>
           <DialogTitle>Add New Agent</DialogTitle>
           <DialogCloseTrigger />
-          <Controller
-            control={control}
-            name="type"
-            render={({ field }) => (
-              <SegmentedControl
-                mt={2}
-                w="full"
-                value={field.value}
-                onValueChange={({ value }) =>
-                  field.onChange(value as AgentType)
-                }
-                items={Object.entries(agentTypes).map(([key, value]) => ({
-                  label: (
-                    <HStack gap="2">
-                      {value.icon}
-                      {value.label}
-                    </HStack>
-                  ),
-                  value: key,
-                }))}
-              />
-            )}
+          <SegmentedControl
+            mt={2}
+            w="full"
+            value={type}
+            onValueChange={({ value }) => setType(value as AgentType)}
+            items={Object.entries(agentTypes).map(([key, value]) => ({
+              label: (
+                <HStack gap="2">
+                  {value.icon}
+                  {value.label}
+                </HStack>
+              ),
+              value: key,
+            }))}
           />
-          <DialogDescription>
+          <DialogDescription pt={2}>
             Remember to add the agent to <Code>config.yml</Code> after adding
             it.
           </DialogDescription>
@@ -127,51 +121,76 @@ export function AddAgentDialogButton() {
               />
             </Group>
             <Group>
-              <Label>Nightly</Label>
-              <Controller
-                control={control}
-                name="nightly"
-                render={({ field }) => (
-                  <Checkbox
-                    w="full"
-                    checked={field.value}
-                    onCheckedChange={({ checked }) => field.onChange(checked)}
-                  />
-                )}
+              <Label>Explicit Only</Label>
+              <Checkbox
+                w="full"
+                checked={explicitOnly}
+                onCheckedChange={({ checked }) =>
+                  setExplicitOnly(checked === true)
+                }
               />
+              <ToggleTip
+                content={
+                  <Text>
+                    When enabled, only routes with GoDoxy labels{" "}
+                    <Code>proxy.*</Code> will be proxied by GoDoxy
+                  </Text>
+                }
+              >
+                <Button size="xs" variant="ghost">
+                  <LuInfo />
+                </Button>
+              </ToggleTip>
             </Group>
+
+            {type === "docker" && (
+              <Group>
+                <Label>Nightly</Label>
+                <Controller
+                  control={control}
+                  name="nightly"
+                  render={({ field }) => (
+                    <Checkbox
+                      w="full"
+                      checked={field.value}
+                      onCheckedChange={({ checked }) => field.onChange(checked)}
+                    />
+                  )}
+                />
+                <ToggleTip content="Nightly builds are less stable and may contain bugs">
+                  <Button size="xs" variant="ghost">
+                    <LuInfo />
+                  </Button>
+                </ToggleTip>
+              </Group>
+            )}
           </Stack>
         </DialogBody>
         <DialogFooter>
-          <Controller
-            control={control}
-            name="type"
-            render={({ field }) => (
-              <Button
-                variant={"ghost"}
-                loading={copyLoading}
-                loadingText="Creating certificates"
-                onClick={handleSubmit((form) => {
-                  setCopyLoading(true);
-                  newAgent(form)
-                    .then(async (e) => {
-                      await navigator.clipboard.writeText(e.compose);
-                      toaster.create({
-                        title: "Copied to clipboard",
-                      });
-                      setAgent(e);
-                    })
-                    .catch(toastError)
-                    .finally(() => setCopyLoading(false));
-                })}
-              >
-                Copy
-                {field.value === "docker"
-                  ? " docker compose"
-                  : " shell command"}
-              </Button>
-            )}
-          />
+          <Button
+            variant={"ghost"}
+            loading={copyLoading}
+            loadingText="Creating certificates"
+            onClick={handleSubmit((form) => {
+              if (explicitOnly) {
+                form.name += "!";
+              }
+              setCopyLoading(true);
+              newAgent({ ...form, type })
+                .then(async (e) => {
+                  await navigator.clipboard.writeText(e.compose);
+                  toaster.create({
+                    title: "Copied to clipboard",
+                  });
+                  setAgent(e);
+                })
+                .catch(toastError)
+                .finally(() => setCopyLoading(false));
+            })}
+          >
+            Copy
+            {type === "docker" ? " docker compose" : " shell command"}
+          </Button>
           <Button
             borderRadius={"lg"}
             disabled={!agent}
