360ff112f7d677c1713063e7e1ed9210f66006b1
yusing <yusing@6uo.me>
2025-03-25 06:02:34 +0800

    better wiki sidebar

--- src/components/wiki_page.tsx
+++ src/components/wiki_page.tsx
@@ -1,13 +1,6 @@
 "use client";
-import {
-  Box,
-  Card,
-  ClientOnly,
-  For,
-  HStack,
-  Link,
-  Stack,
-} from "@chakra-ui/react";
+import { bodyHeight } from "@/styles";
+import { Box, For, HStack, Link, Table } from "@chakra-ui/react";
 import MarkdownPreview from "@uiw/react-markdown-preview";
 import { usePathname } from "next/navigation";
 import { useAsync } from "react-use";
@@ -17,7 +10,6 @@ export default function WikiPage({ file }: Readonly<{ file: string }>) {
   const home = useAsync(async () => {
     return await fetch(`/wiki/${file}.md`).then((res) => res.text());
   });
-  const pathname = usePathname();
   return (
     <HStack gap="4" align={"start"} justify={"space-between"}>
       <Box overflow={"auto"}>
@@ -27,31 +19,54 @@ export default function WikiPage({ file }: Readonly<{ file: string }>) {
           style={{ padding: 24, background: "transparent" }}
         />
       </Box>
-      <Card.Root position={"sticky"} top={0} right={"0"}>
-        <Card.Body>
-          <ClientOnly>
-            <Stack gap="4">
-              <For each={sideBar}>
-                {(item) => (
-                  <HStack key={item.name}>
-                    <Link
-                      href={item.link}
-                      fontWeight={"medium"}
-                      colorPalette={
-                        pathname.slice("/docs/".length) === item.link
-                          ? "purple"
-                          : "blue"
-                      }
-                    >
-                      {item.name}
-                    </Link>
-                  </HStack>
-                )}
-              </For>
-            </Stack>
-          </ClientOnly>
-        </Card.Body>
-      </Card.Root>
+      <WikiSidebar />
     </HStack>
+  );
+}
+
+function WikiSidebar() {
+  const pathname = usePathname();
+  return (
+    <Box
+      position={"sticky"}
+      top={0}
+      right={"0"}
+      m={4}
+      h={bodyHeight}
+      w={"200px"}
+      overflow={"auto"}
+      border={"1px solid"}
+      borderColor={"border.emphasized"}
+      borderRadius={"md"}
+    >
+      <Table.Root size="sm" stickyHeader>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>Pages</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <For each={sideBar}>
+            {(item) => (
+              <Table.Row key={item.name}>
+                <Table.Cell>
+                  <Link
+                    href={item.link}
+                    fontWeight={"medium"}
+                    colorPalette={
+                      pathname.slice("/docs/".length) === item.link
+                        ? "purple"
+                        : "blue"
+                    }
+                  >
+                    {item.name}
+                  </Link>
+                </Table.Cell>
+              </Table.Row>
+            )}
+          </For>
+        </Table.Body>
+      </Table.Root>
+    </Box>
   );
 }
