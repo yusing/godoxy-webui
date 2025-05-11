2c2bfa12a228f6cc36180d6d485f94709f295fee
yusing <yusing@6uo.me>
2025-05-11 16:54:09 +0800

    fix: oidc invalid oauth state

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,28 +1,22 @@
 "use client";
 
 import Endpoints from "@/types/api/endpoints";
-import { StatusCodes } from "http-status-codes";
-import { FC, PropsWithChildren, useEffect } from "react";
+import { useEffect } from "react";
 
-export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
+// weird redirect behavior with fetch
+// with 'manual' redirect, the status code will be 0 and headers will be empty
+// We need to use 'follow' redirect to get the correct status code and redirect URL
+// https://github.com/whatwg/fetch/issues/763
+export function AuthProvider() {
   useEffect(() => {
-    fetch(Endpoints.AUTH_CHECK, { redirect: "error" })
-      .then((r) => {
-        if (
-          r.status === StatusCodes.FORBIDDEN ||
-          r.status === StatusCodes.UNAUTHORIZED
-        ) {
-          redirect();
-        }
-      })
-      .catch(redirect);
+    fetch(Endpoints.AUTH_CHECK, {
+      method: "HEAD",
+      redirect: "follow",
+    }).then(async (r) => {
+      if (r.redirected) {
+        window.location.href = r.url;
+      }
+    });
   });
-
-  return children;
-};
-
-function redirect() {
-  if (window.location.pathname != "/login") {
-    window.location.href = Endpoints.AUTH_REDIRECT;
-  }
+  return null;
 }
