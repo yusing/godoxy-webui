6a199c050fa5670037707123cd81eb16c8d75db3
yusing <yusing@6uo.me>
2025-05-11 23:39:23 +0800

    fix: auth check

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import Endpoints from "@/types/api/endpoints";
-import { useEffect } from "react";
+import { useMount } from "react-use";
 
 // weird redirect behavior with fetch
 // with 'manual' redirect, the status code will be 0 and headers will be empty
 // We need to use 'follow' redirect to get the correct status code and redirect URL
 // https://github.com/whatwg/fetch/issues/763
 export function AuthProvider() {
-  useEffect(() => {
+  useMount(() => {
     fetch(Endpoints.AUTH_CHECK, {
       method: "HEAD",
       redirect: "follow",
