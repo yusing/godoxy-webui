81e799dd545b0751ed013e20318c1a2ee08341e0
yusing <yusing@6uo.me>
2025-09-13 00:08:51 +0800

    fix(ui): update outline button variant to use ring

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           'bg-destructive/50 dark:bg-destructive/40 text-destructive-foreground shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40',
         outline:
-          'border border-input shadow-xs hover:bg-accent hover:text-accent-foreground dark:hover:bg-input/50',
+          'ring ring-border shadow-xs hover:bg-accent hover:text-accent-foreground dark:hover:bg-input/50',
         secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
