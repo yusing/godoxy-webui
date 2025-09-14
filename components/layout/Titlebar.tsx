fc02779740a9f2ce26939563e67aed6bc78723b3
yusing <yusing@6uo.me>
2025-09-14 12:12:58 +0800

    refactor(Titlebar): hide text on mobile and add desktop-only visibility for config link

--- components/layout/Titlebar.tsx
+++ components/layout/Titlebar.tsx
@@ -1,4 +1,5 @@
 import { ModeToggle } from '@/components/ui/theme-toggle'
+import { cn } from '@/lib/utils'
 import Logo from '@/public/godoxy-no-text.png'
 import { siteConfig } from '@/site-config'
 import { BookOpenText, Code, Grid3X3, Route, Server } from 'lucide-react'
@@ -7,9 +8,16 @@ import Link from 'next/link'
 import LogoutButton from '../LogoutButton'
 import VersionText from '../VersionText'
 
-const pages = [
+type Page = {
+  href: string
+  Icon: React.ElementType
+  label: string
+  desktopOnly?: boolean
+}
+
+const pages: Page[] = [
   { href: '/', Icon: Grid3X3, label: 'Apps' },
-  { href: '/config', Icon: Code, label: 'Config' },
+  { href: '/config', Icon: Code, label: 'Config', desktopOnly: true },
   { href: '/routes', Icon: Route, label: 'Routes' },
   { href: '/servers', Icon: Server, label: 'Servers' },
   { href: '/wiki/Home', Icon: BookOpenText, label: 'Wiki' },
@@ -26,14 +34,12 @@ export default function Titlebar() {
         <span className="font-semibold tracking-tight">{siteConfig.metadata.title}</span>
         <VersionText />
         <div className="flex items-center ml-4 gap-4 text-sm text-muted-foreground">
-          {pages.map(({ href, Icon, label }) => (
-            <Item key={href} href={href} icon={<Icon className="size-4" />}>
-              {label}
-            </Item>
+          {pages.map(page => (
+            <Item key={page.href} {...page} />
           ))}
         </div>
       </div>
-      <div className="flex items-center gap-4 text-sm text-muted-foreground">
+      <div className="flex items-center gap-2 sm:gap-4 text-sm text-muted-foreground">
         <ModeToggle />
         <LogoutButton className="inline-flex items-center gap-2 hover:text-foreground" />
       </div>
@@ -41,19 +47,19 @@ export default function Titlebar() {
   )
 }
 
-function Item({
-  children,
-  icon,
-  href,
-}: {
-  children: React.ReactNode
-  icon: React.ReactNode
-  href: string
-}) {
+function Item({ href, Icon, label, desktopOnly }: Page) {
   return (
-    <Link prefetch={false} href={href} className="titlebar-item data-[active=true]:text-primary">
-      <div className="inline-flex items-center gap-2 hover:text-foreground">
-        {icon} {children}
+    <Link
+      prefetch={false}
+      href={href}
+      className={cn(
+        'titlebar-item data-[active=true]:text-primary',
+        desktopOnly && 'hidden sm:inline'
+      )}
+      title={label}
+    >
+      <div className="flex items-center gap-2 hover:text-foreground">
+        <Icon className="size-4" /> <span className="hidden sm:inline">{label}</span>
       </div>
     </Link>
   )
