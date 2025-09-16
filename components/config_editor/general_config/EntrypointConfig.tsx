e9d6c7df16ab6339cf0af3ba7f162aed41a3ae2f
yusing <yusing@6uo.me>
2025-09-16 18:02:00 +0800

    feat(EntrypointConfig): add access log configuration and improve middleware handling

--- components/config_editor/general_config/EntrypointConfig.tsx
+++ components/config_editor/general_config/EntrypointConfig.tsx
@@ -1,11 +1,40 @@
+import { MapInput } from '@/components/form/MapInput'
 import { NamedListInput } from '@/components/form/NamedListInput'
-import { MiddlewareComposeSchema } from '@/types/godoxy'
+import { ConfigSchema, MiddlewareComposeSchema } from '@/types/godoxy'
 import type { MiddlewareFileRef } from '@/types/godoxy/middlewares/middlewares'
 import { useMemo } from 'react'
 import { middlewareUseToSnakeCase } from '../middleware_compose/MiddlewareEditor'
 import { configStore } from '../store'
 
 export default function EntrypointConfigContent() {
+  return (
+    <div className="flex flex-col gap-6">
+      <EntrypointAccessLogConfig />
+      <EntrypointMiddlewaresConfig />
+    </div>
+  )
+}
+
+// FIXME: these schema causes UI to freeze
+const accessLogSchema = ConfigSchema.definitions.RequestLogConfig
+// @ts-expect-error TODO: fix this
+delete accessLogSchema.properties.filters
+// @ts-expect-error TODO: fix this
+delete accessLogSchema.properties.fields
+
+function EntrypointAccessLogConfig() {
+  const accessLog = configStore.configObject.entrypoint.access_log.value
+  return (
+    <MapInput
+      label="Access log"
+      schema={accessLogSchema}
+      value={accessLog}
+      onChange={configStore.configObject.entrypoint.access_log.set}
+    />
+  )
+}
+
+function EntrypointMiddlewaresConfig() {
   const middlewares = configStore.configObject.entrypoint.middlewares.use()
   const workingValue = useMemo(() => {
     return (middlewares ?? []).map(item => ({
@@ -16,8 +45,7 @@ export default function EntrypointConfigContent() {
 
   return (
     <NamedListInput
-      card={false}
-      label=""
+      label="Middlewares"
       nameField="use"
       keyField="use"
       schema={MiddlewareComposeSchema.definitions.MiddlewareComposeItem}
