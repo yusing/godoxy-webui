1058987e8b1b12d58702cad239725bf0bffb93d6
yusing <yusing@6uo.me>
2025-09-19 10:49:22 +0800

    chore(schema): add support_proxy_protocol field in EntrypointConfig

--- types/godoxy/config/entrypoint.ts
+++ types/godoxy/config/entrypoint.ts
@@ -2,6 +2,8 @@ import type { EntrypointMiddlewares } from '../middlewares/middleware_compose'
 import type { RequestLogConfig } from './access_log'
 
 export type EntrypointConfig = {
+  /** Enable support for proxy protocol */
+  support_proxy_protocol?: boolean
   /** Entrypoint middleware configuration */
   middlewares?: EntrypointMiddlewares
   /** Entrypoint access log configuration */
