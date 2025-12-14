09577e1ffe3bb8fdc06b6fe9f9167f5de9120d88
yusing <yusing.wys@gmail.com>
2025-12-15 00:28:23 +0800

    feat(routes): add quick toggles to route list; make UI more compact

--- components/config_editor/route_files/RouteDisplay.tsx
+++ components/config_editor/route_files/RouteDisplay.tsx
@@ -1,7 +1,14 @@
 import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Field, FieldLabel } from '@/components/ui/field'
+import { StoreCheckboxField } from '@/juststore-shadcn/src/components/store/Checkbox'
+import { isEqual } from '@/juststore/src/impl'
 import { cn } from '@/lib/utils'
 import type { Routes } from '@/types/godoxy'
+import type { CIDR } from '@/types/godoxy/types'
+import type { State } from 'juststore'
 import { ArrowRight, Copy, Edit2, Globe, Trash } from 'lucide-react'
+import { routesConfigStore as store } from '../store'
 import RouteIcon from './RouteIcon'
 import * as utils from './utils'
 
@@ -24,16 +31,16 @@ export default function RouteDisplay({
   const [iconColor, textColor] = utils.getIconColorsByScheme(route.scheme)
 
   return (
-    <div className="space-y-3">
+    <div className="space-y-2.5">
       {/* Header with icon, title and edit button */}
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
-          <div className={cn('shrink-0 rounded-lg p-2 border', iconColor)}>
-            <RouteIcon scheme={route.scheme ?? 'http'} className={cn(`h-5 w-5`, textColor)} />
+          <div className={cn('shrink-0 rounded-lg p-1.5 border', iconColor)}>
+            <RouteIcon scheme={route.scheme ?? 'http'} className={cn(`size-4`, textColor)} />
           </div>
           <div>
-            <h3 className="font-semibold text-lg">{alias}</h3>
-            <p className="text-muted-foreground text-sm">
+            <h3 className="font-semibold text">{alias}</h3>
+            <p className="text-muted-foreground text-xs">
               {utils.getRouteType(route.scheme) + (alias.startsWith('x-') ? ' Template' : '')}
             </p>
           </div>
@@ -52,7 +59,7 @@ export default function RouteDisplay({
       </div>
 
       {/* Connection details */}
-      <div className="flex items-center gap-2 text-muted-foreground text-sm">
+      <div className="flex items-center gap-2 text-muted-foreground text-xs">
         <Globe className="h-4 w-4" />
 
         {listeningAddress && (
@@ -63,6 +70,120 @@ export default function RouteDisplay({
         )}
         <span className="font-mono">{proxyAddress}</span>
       </div>
+
+      <QuickSettings alias={alias} />
     </div>
+  )
+}
+
+const DEFAULT_CIDR_ALLOW = [
+  '192.168.0.0/16',
+  '10.0.0.0/16',
+  '127.0.0.1/32',
+  '100.64.0.0/12',
+] as CIDR[]
+
+function QuickSettings({ alias }: { alias: string }) {
+  return (
+    <div className="flex gap-2 bg-card py-1 px-2 rounded-md">
+      <LANOnlyToggle alias={alias} />
+      <HealthCheckToggle alias={alias} />
+      <ShowOnDashboardToggle alias={alias} />
+    </div>
+  )
+}
+
+function LANOnlyToggle({ alias }: { alias: string }) {
+  const isReverseProxy = store.useCompute(
+    `configObject.${alias}.scheme`,
+    s => utils.getRouteType(s) === 'Reverse Proxy'
+  )
+  const [matchesDefault, hasCustom, ariaChecked] = store.useCompute(
+    `configObject.${alias}.middlewares.cidr_whitelist.allow`,
+    m => {
+      const matchesDefault = isEqual(m, DEFAULT_CIDR_ALLOW)
+      const hasCustom = m && !matchesDefault
+      return [
+        matchesDefault,
+        hasCustom,
+        hasCustom ? 'mixed' : matchesDefault ? 'true' : 'false',
+      ] as const
+    }
+  )
+
+  const handleChange = (checked: boolean) => {
+    if (checked) {
+      if (matchesDefault) {
+        return
+      }
+      store.set(`configObject.${alias}.middlewares.cidr_whitelist.allow`, DEFAULT_CIDR_ALLOW)
+    } else {
+      store.reset(`configObject.${alias}.middlewares.cidr_whitelist`)
+    }
+  }
+
+  if (!isReverseProxy) {
+    return null
+  }
+
+  return (
+    <Field
+      orientation="horizontal"
+      title={
+        hasCustom
+          ? undefined
+          : matchesDefault
+            ? 'Remove default CIDR whitelist'
+            : 'Add default CIDR whitelist'
+      }
+    >
+      <Checkbox
+        id="lan-only"
+        checked={matchesDefault}
+        disabled={hasCustom}
+        onCheckedChange={handleChange}
+        aria-checked={ariaChecked}
+      />
+      <FieldLabel htmlFor="lan-only">
+        LAN Only
+        {hasCustom && (
+          <p className="text-xs text-muted-foreground">
+            Custom CIDR whitelist detected, manual update is required
+          </p>
+        )}
+      </FieldLabel>
+    </Field>
+  )
+}
+
+function HealthCheckToggle({ alias }: { alias: string }) {
+  return (
+    <StoreCheckboxField
+      state={store.configObject![alias]!.healthcheck.disable}
+      title="No Health Check"
+      labelPlacement="right"
+    />
+  )
+}
+
+function ShowOnDashboardToggle({ alias }: { alias: string }) {
+  const isReverseProxy = store.useCompute(
+    `configObject.${alias}.scheme`,
+    s => utils.getRouteType(s) === 'Reverse Proxy'
+  )
+  if (!isReverseProxy) {
+    return null
+  }
+  return (
+    <StoreCheckboxField
+      state={
+        (store.state(`configObject.${alias}.homepage.show`) as State<boolean>).derived({
+          from: value => value ?? true, // default to true if undefined
+          to: value => !!value,
+        }) as State<boolean>
+      }
+      title="Show on Dashboard"
+      labelPlacement="right"
+    />
   )
 }
