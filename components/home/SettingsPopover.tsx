352d91e905e9d1f32b81e85662203b58b40da9bb
yusing <yusing.wys@gmail.com>
2025-12-15 15:50:15 +0800

    feat(SettingsPopover): enhance theme-aware icon settings with detailed description and updated labels

--- components/home/SettingsPopover.tsx
+++ components/home/SettingsPopover.tsx
@@ -6,7 +6,7 @@ import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover
 import { RadioGroup, RadioGroupField } from '@/components/ui/radio-group'
 import { Separator } from '@/components/ui/separator'
 import { StoreSwitchField } from '@/juststore-shadcn/src/components/store/Switch'
-import { Settings } from 'lucide-react'
+import { Moon, Settings, Sun } from 'lucide-react'
 import { store } from './store'
 
 export default function SettingsPopover() {
@@ -59,8 +59,19 @@ export default function SettingsPopover() {
             <Label className="text-xs font-medium">UI Preferences</Label>
             <StoreSwitchField
               state={store.ui.iconThemeAware}
-              title="Icon Theme Aware"
+              title="Theme-Aware Icons"
               labelPlacement="right"
+              description={
+                <div className="space-y-1">
+                  <p>Prefer monochrome light or dark icons based on the current theme.</p>
+                  <div className="flex items-center gap-1">
+                    <span className="bg-white text-black p-0.5 border rounded-md">Light icons</span>
+                    on <Moon className="size-3 inline-block" /> night mode,
+                    <span className="bg-black text-white p-0.5 border rounded-md">dark icons</span>
+                    on <Sun className="size-3 inline-block" /> light mode.
+                  </div>
+                </div>
+              }
             />
           </div>
           <store.ui.showKeyboardHints.Render>
