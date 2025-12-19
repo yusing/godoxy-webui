07c3e3643efd356c0052d6780c5c3b826f23b41f
yusing <yusing.wys@gmail.com>
2025-12-19 17:39:47 +0800

    refactor(styles): update global styles and font imports; replace default fonts with Geist fonts; remove CascadiaCode font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,23 @@ import ReactScan from '@/components/ReactScan'
 import Titlebar from '@/components/layout/Titlebar'
 import TitlebarController from '@/components/layout/TitlebarController'
 import { siteConfig } from '@/site-config'
-import '@fontsource/cascadia-code'
 import { ThemeProvider } from 'next-themes'
+import { Geist, Geist_Mono } from 'next/font/google'
 import type { ReactNode } from 'react'
 import { Toaster } from 'sonner'
 import './globals.css'
 
 export const metadata = siteConfig.metadata
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export default function RootLayout({
   children,
@@ -17,12 +27,12 @@ export default function RootLayout({
   children: ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={geistSans.variable}>
       <ReactScan />
       <head>
         <meta name="apple-mobile-web-app-title" content="GoDoxy" />
       </head>
-      <body className="antialiased">
+      <body className={`${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="system" storageKey="ui-theme" enableSystem>
           <Titlebar />
           <TitlebarController />
