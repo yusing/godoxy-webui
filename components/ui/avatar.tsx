5add74717ac0da44bbf1ea0afffcf74ae6a3cdc5
yusing <yusing.wys@gmail.com>
2025-12-20 19:47:51 +0800

    fix(avatar): remove border from after pseudo-element

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -14,7 +14,7 @@ function Avatar({
       data-slot="avatar"
       data-size={size}
       className={cn(
-        'size-8 rounded-full after:rounded-full data-[size=lg]:size-10 data-[size=sm]:size-6 after:border-border group/avatar relative flex shrink-0 select-none after:absolute after:inset-0 after:border after:mix-blend-darken dark:after:mix-blend-lighten',
+        'size-8 rounded-full after:rounded-full data-[size=lg]:size-10 data-[size=sm]:size-6 after:border-none group/avatar relative flex shrink-0 select-none after:absolute after:inset-0 after:border after:mix-blend-darken dark:after:mix-blend-lighten',
         className
       )}
       {...props}
